e406b00b24651a22eade646687e320709b5bf33a
Alex Forrence <aforren1@jhu.edu>
2018-04-09 18:17:35 -0400

    Updates for recording

--- plotosaurus.py
+++ plotosaurus.py
@@ -1,36 +1,30 @@
-import sys
+import argparse
+import ctypes
 import datetime
+import sys
+
 import numpy as np
 import pyqtgraph as pg
+from pyqtgraph.dockarea import Dock, DockArea
 from pyqtgraph.Qt import QtCore, QtGui
-from pyqtgraph.dockarea import DockArea, Dock
+
 from toon.input import MultiprocessInput as MpI
+from toon.input.fake import FakeInput
 from toon.input.hand import Hand
-# pip install git+https://github.com/aforren1/toon
 
-np.set_printoptions(precision=4, linewidth=150, suppress=True, threshold=1500)
-
-class Dummy():
-    def __init__(self):
-        pass
-    def __enter__(self):
-        pass
-    def __exit__(self, *args):
-        pass
-
-demo = False
+np.set_printoptions(precision=4, linewidth=150, suppress=True)
 
 # base window, etc.
 app = QtGui.QApplication([])
 win = QtGui.QMainWindow()
-#win.setWindowIcon(QtGui.QIcon('icon.jpg'))
+
 area = DockArea()
 win.setCentralWidget(area)
 win.resize(1200, 400)
 win.setWindowTitle('The Plotosaurus')
 
-d1 = Dock('Forces', size = (1000, 600))
-d2 = Dock('Settings', size = (250, 600))
+d1 = Dock('Forces', size=(1000, 600))
+d2 = Dock('Settings', size=(250, 600))
 area.addDock(d1, 'left')
 area.addDock(d2, 'right')
 
@@ -41,47 +35,63 @@ curves = list()
 
 for i in range(num_plots):
     plots.append(plotwidget.addPlot())
-    #plots[i].setDownsampling(mode='peak')
     plots[i].setClipToView(True)
     plots[i].setRange(yRange=[-1, 1])
     data = np.random.normal(size=200)
     for j in range(3):
-        curves.append(plots[i].plot(data, pen=pg.mkPen(color=pg.intColor(j, hues=5, alpha=255, width=3))))
+        curves.append(plots[i].plot(data, pen=pg.mkPen(
+            color=pg.intColor(j, hues=5, alpha=255, width=3))))
 
 d1.addWidget(plotwidget)
 
 logging = False
 log_file_name = ''
-def log_and_print(state):
+
+def stop_logging():
+    global logging
+    logging = False
+    logging_toggler.setText('Log for 5 seconds')
+
+
+timer = QtCore.QTimer()
+
+def log_and_print():
     global logging, log_file_name
-    logging = state
-    log_file_name = datetime.datetime.now().strftime('log_%Y-%m-%d_%H-%M-%S.txt')
+    logging = True
+    log_file_name = textbox.text(
+    ) + datetime.datetime.now().strftime('_%Y-%m-%d_%H-%M-%S') + '.txt'
+    logging_toggler.setText('Now logging')
+    timer.singleShot(5000, stop_logging)
 
 please_center = False
+
+
 def median_centering():
     global please_center
     please_center = True
 
+
 setwidget = pg.LayoutWidget()
-logging_toggler = QtGui.QCheckBox('Logging')
-logging_toggler.stateChanged.connect(log_and_print)
+logging_toggler = QtGui.QPushButton('Log for 5 seconds')
+logging_toggler.clicked.connect(log_and_print)
 zero_button = QtGui.QPushButton('Center Traces')
 zero_button.clicked.connect(median_centering)
 
 setwidget.addWidget(logging_toggler)
 setwidget.addWidget(zero_button)
 d2.addWidget(setwidget)
+textbox = QtGui.QLineEdit()
+setwidget.addWidget(textbox)
 
 centering = None
 current_data_view = None
+
+
 def update():
     global current_data_view, logging, log_file_name, centering, please_center
-    if demo:
-        data = np.random.random((16, 15))
-    else:
-        ts, data = dev.read()
-        if data is None:
-            return
+    ts, data = dev.read()
+    if data is None:
+        return
     if please_center:
         please_center = False
         centering = np.median(data, axis=0)
@@ -92,20 +102,26 @@ def update():
         current_data_view = np.vstack((current_data_view, data - centering))
     else:
         current_data_view = np.roll(current_data_view, -data.shape[0], axis=0)
-        current_data_view[-data.shape[0]:,:] = data - centering
+        current_data_view[-data.shape[0]:, :] = data - centering
     if logging:
-        print(data[-1,:])
+        print(data[-1, :])
         with open(log_file_name, 'ab') as f:
             np.savetxt(f, data, fmt='%10.5f', delimiter=',')
     for counter, c in enumerate(curves):
         c.setData(y=current_data_view[:, counter])
 
+
 if __name__ == '__main__':
-    if demo:
-        device = Dummy()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('demo', help='Demo mode or not',
+                        type=bool, default=True)
+    args = parser.parse_args()
+    if args.demo:
+        device = MpI(FakeInput, sampling_frequency=1000,
+                     data_shape=[[15]], data_type=[ctypes.c_double])
     else:
         device = MpI(Hand)
-    
+
     with device as dev:
         win.show()
         timer = pg.QtCore.QTimer()
